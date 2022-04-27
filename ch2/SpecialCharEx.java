c3538b5d4649a5ce2ac01bb02e6a7b3d31c47605
soo <sooyoungs0120@gmail.com>
2022-04-27 14:49:15 +0900

    Chapter 1, 2, 3, 6 example code

---
+++ ch2/SpecialCharEx.java
@@ -0,0 +1,11 @@
+package ch2;
+
+class SpecialCharEx {
+    public static void main(String[] args) {
+        System.out.println('\'');   // '
+        System.out.println("abc\t123\b456");  // abc	12456
+        System.out.println('\n');   // 줄바꿈
+        System.out.println("\"hello\"");    // "Hello"
+        System.out.println("c:\\"); // c:\
+    }
+}
---
+++ ch3/OperatorEx13.java
@@ -0,0 +1,13 @@
+package ch3;
+
+class OperatorEx13 {
+    public static void main(String[] args) {
+        char c1 = 'a';
+
+//        char errorC2 = c1 + 1;
+        char c2 = 'a' + 1;
+//        -> char c2 = 'b' 로 컴파일함
+
+        System.out.println(c2);
+    }
+}
